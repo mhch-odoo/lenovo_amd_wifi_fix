f0eabb43fdd47df09b3a89e6d0fee212e8df5f39
mhch-odoo <mhch@odoo.com>
2025-11-25 14:29:30 +1000

    Hardcode the file instead of curl

--- ubuntu-wifi-power-management.sh
+++ ubuntu-wifi-power-management.sh
@@ -35,6 +35,10 @@ apt install -y lshw
 # Check wireless interface driver
 WIFI_DRIVER=$(lshw -C network | grep -A 12 "Wireless interface" | grep 'driver=' | grep -o 'driver=[^ ]*' | cut -d'=' -f2 | head -n 1)
 DRIVER_VERSION=$(modinfo $WIFI_DRIVER | grep 'firmware:')
+
+sed -i 's/wifi.powersave = 3/wifi.powersave = 2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+log 2 "Disabled power saving for Wi-Fi in NetworkManager configuration."
+
 if [ "$WIFI_DRIVER" != "ath11k_pci" ]; then
     log 2 "This script is intended for systems using the ath11k_pci Wi-Fi driver. If you still encounter Wi-Fi issues, please contact mhch@odoo.com with the following info."
     log 1 "Detected Wi-Fi driver: $WIFI_DRIVER \n$DRIVER_VERSION."
@@ -44,6 +48,7 @@ fi
 # Install necessary packages
 apt install -y iw wireless-tools
 
+log 1 "Disabling power management for Wi-Fi interface: $WIFI_INTERFACE"
 # Disable Wi-Fi power management
 WIFI_INTERFACE=$(iw dev | awk '$1=="Interface"{print $2}' | head -n 1)
 if [ -z "$WIFI_INTERFACE" ]; then
@@ -51,8 +56,6 @@ if [ -z "$WIFI_INTERFACE" ]; then
     exit 1
 fi
 
-log 1 "Disabling power management for Wi-Fi interface: $WIFI_INTERFACE"
-sed -i 's/wifi.powersave = 3/wifi.powersave = 2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
 iwconfig $WIFI_INTERFACE power off
 
 # Restart nm to apply changes
@@ -62,15 +65,47 @@ log 2 "Wi-Fi power management has been disabled."
 
 # Clone the to reset the ath11k_pci module on suspend/resume
 log 1 "Cloning reset-wifi-module.sh script..."
-TEMP_DIR=$(mktemp -d)
-curl -fsSL -o $TEMP_DIR/reset-wifi-module.sh https://raw.githubusercontent.com/s-damian/thinkpad-t14-gen-5-amd-linux/main/sh/reset-wifi-module.sh
-if [ $? -ne 0 ]; then
-    log 3 "Failed to download reset-wifi-module.sh. Exiting..."
-    rm -rf $TEMP_DIR
-    exit 1
-fi
-mv $TEMP_DIR/reset-wifi-module.sh /lib/systemd/system-sleep/reset-wifi-module.sh
-rm -rf $TEMP_DIR
+
+# Clones the script from https://github.com/s-damian/thinkpad-t14-gen-5-amd-linux
+
+cat << 'EOF' > /lib/systemd/system-sleep/reset-wifi-module.sh
+#!/bin/bash
+
+# For path: /lib/systemd/system-sleep/reset-wifi-module.sh
+
+# This script unloads and reloads the Qualcomm WiFi module around suspend
+
+case "$1" in
+    pre)
+        logger "Reset-WiFi-Module-Script : ---------- [PRE-SUSPEND START] ----------"
+
+        # Before suspend: unload the Qualcomm WiFi module
+        logger "Reset-WiFi-Module-Script : [PRE-SUSPEND START] Unloading Qualcomm WiFi module:"
+        modprobe -r ath11k_pci
+        if [ $? -eq 0 ]; then
+            logger "Reset-WiFi-Module-Script : [PRE-SUSPEND SUCCESS] Qualcomm module unloaded successfully."
+        else
+            logger "Reset-WiFi-Module-Script : [PRE-SUSPEND ERROR] Failed to unload Qualcomm module."
+        fi
+
+        logger "Reset-WiFi-Module-Script : ---------- [PRE-SUSPEND END] ----------"
+        ;;
+    post)
+        logger "Reset-WiFi-Module-Script : ---------- [POST-RESUME START] ----------"
+
+        # After resume: reload the Qualcomm WiFi module
+        logger "Reset-WiFi-Module-Script : [POST-RESUME START] Reloading Qualcomm WiFi module:"
+        modprobe ath11k_pci
+        if [ $? -eq 0 ]; then
+            logger "Reset-WiFi-Module-Script : [POST-RESUME SUCCESS] Qualcomm module reloaded successfully."
+        else
+            logger "Reset-WiFi-Module-Script : [POST-RESUME ERROR] Failed to reload Qualcomm module."
+        fi
+
+        logger "Reset-WiFi-Module-Script : ---------- [POST-RESUME END] ----------"
+        ;;
+esac
+EOF
 
 chmod +x /lib/systemd/system-sleep/reset-wifi-module.sh
 chown root:root /lib/systemd/system-sleep/reset-wifi-module.sh
